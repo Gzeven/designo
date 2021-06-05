bbbe5f454e4ffce1379606da251a4b8e7072780e
Gerben van de Weerdhof <gerben1978@hotmail.com>
2021-06-05 15:06:37 +0200

    background fixed

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -16,6 +16,7 @@ const Wrapper = styled.main`
   height: 100vh;
   color: var(--peach);
   background-image: url('https://images.unsplash.com/photo-1617631717736-3263609d2375?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80');
+  background-position: calc(50% - 0px/2) calc(50% - 0px/2);
   background-repeat: no-repeat;
   background-size: cover;
   display: flex;
@@ -24,6 +25,14 @@ const Wrapper = styled.main`
   text-align: center;
   h3 {
     margin-bottom: 1.5rem;
+  }
+
+  @media (min-width: 768px) {
+    background-position: calc(50% - 0px/2) calc(50% - 0px/2);
+  }
+
+@media (min-width: 1281px) {
+   background-position: calc(50% - 0px/2) calc(50% - 0px/2);
   }
   
 `
